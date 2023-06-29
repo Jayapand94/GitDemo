5de78593177e56bde05ea3b2fa1f9d5c805a71d6
ELCOT <ELCOT@Lenovo>
2023-06-29 11:29:36 +0530

    Commit 1

---
+++ New folder (2)/Selenium Project/src/com/selenium/org/Reverse_String.java
@@ -0,0 +1,21 @@
+package com.selenium.org;
+
+public class Reverse_String {
+	
+	public static void main(String[] args) {
+		
+		String s="Automation";
+		String r=" ";
+		char c;
+		for (int i = s.length()-1; i >=0; i--) {
+			
+			c=s.charAt(i);
+			
+			r=r+c;
+		}
+		System.out.println(r);
+		
+		
+	}
+
+}
